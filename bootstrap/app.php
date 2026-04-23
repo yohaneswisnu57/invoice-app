f72dcd67d58f6647037eb61671a471f7ef496792
Yohanes Wisnu <wisnuyohanes33@gmail.com>
2026-04-23 20:23:56 +0700

    feat: initialize application configuration with routing and middleware setup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
